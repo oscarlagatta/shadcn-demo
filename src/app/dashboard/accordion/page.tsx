e1f2294af7bcd71fe16457ff5d970a99ddc1a691
Oscar Nicolas Lagatta <oscarlagatta@gmail.com>
2024-09-21 10:47:52 +0100

    Add accordion component to dashboard
    Integrated @radix-ui/react-accordion to the project and created an accordion component for the dashboard page. Updated Tailwind config for accordion animations and ensured new dependencies are captured in package files.

--- src/app/dashboard/accordion/page.tsx
+++ src/app/dashboard/accordion/page.tsx
@@ -1,6 +1,45 @@
+import {Accordion, AccordionContent, AccordionItem, AccordionTrigger} from "@/components/ui/accordion";
+
+const items = [
+    {
+        id: "item-1",
+        question: "Is it accessible?",
+        answer: "Yes. It adheres to the WAI-ARIA design pattern.",
+    },
+    {
+        id: "item-2",
+        question: "How do I get started?",
+        answer: "You can start by reading the documentation.",
+    },
+    {
+        id: "item-3",
+        question: "Can I use it on multiple projects?",
+        answer: "Yes. It's licensed under the MIT license.",
+    },
+    {
+        id: "item-4",
+        question: "Can I use it on multiple projects?",
+        answer: "Yes. It's licensed under the MIT license.",
+    },
+];
+
+
 function Page() {
     return (
-        <div>Accordion Page</div>
+        <div>
+            {
+                items.map((item) => (
+                    <Accordion type="single" collapsible className="w-full" key={item.id}>
+                        <AccordionItem value={item.id} key={item.id}>
+                            <AccordionTrigger>{item.question}</AccordionTrigger>
+                            <AccordionContent>
+                                {item.answer}
+                            </AccordionContent>
+                        </AccordionItem>
+                    </Accordion>
+                ))
+            }
+        </div>
     );
 }
 
