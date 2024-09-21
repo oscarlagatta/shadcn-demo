cd62aa86150d9420c6784cc6f87bff0240abe8c8
Oscar Nicolas Lagatta <oscarlagatta@gmail.com>
2024-09-21 10:56:41 +0100

    Add alert components to dashboard alert page
    This update introduces custom alert components to the `alert` page in the dashboard, providing `default`, `destructive`, and `success` variants. The new components are defined in a newly created `alert.tsx` file in the `components/ui` directory, enhancing the page with a more functional and user-friendly design.

--- src/app/dashboard/alert/page.tsx
+++ src/app/dashboard/alert/page.tsx
@@ -1,6 +1,35 @@
+import {Alert, AlertDescription, AlertTitle} from "@/components/ui/alert";
+import {RocketIcon} from "@radix-ui/react-icons";
+
 function Page() {
     return (
-        <div>Alert Page</div>
+        <div className="grid gap-3">
+
+            <Alert>
+                <RocketIcon className="h-4 w-4" />
+                <AlertTitle>Heads up!</AlertTitle>
+                <AlertDescription>
+                    You can add components to your app using the cli.
+                </AlertDescription>
+            </Alert>
+            <Alert variant='destructive'>
+                <RocketIcon className="h-4 w-4" />
+                <AlertTitle>Heads up!</AlertTitle>
+                <AlertDescription>
+                    You can add components to your app using the cli.
+                </AlertDescription>
+            </Alert>
+
+            <Alert variant='success'>
+                <RocketIcon className="h-4 w-4" />
+                <AlertTitle>Heads up!</AlertTitle>
+                <AlertDescription>
+                    You can add components to your app using the cli.
+                </AlertDescription>
+            </Alert>
+
+
+        </div>
     );
 }
 
